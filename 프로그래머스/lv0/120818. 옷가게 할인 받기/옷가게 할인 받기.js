3fc3fd716882ca82930a65f279a305af40f6fcd1
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-04-23 23:03:34 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,8 @@
+function solution(price) {
+    var answer = 0;
+    if(price >= 500000) return Math.floor(price*0.8);
+    else if(price>=300000) return Math.floor(price*0.9);
+    else if(price>=100000) return Math.floor(price*0.95);
+    else return price;
+
+}
