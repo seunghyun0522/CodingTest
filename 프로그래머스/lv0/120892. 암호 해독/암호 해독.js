f61325da1feeeb093ca6e60acace6da3c3879217
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-04-28 10:33:53 +0900

    [level 0] Title: 암호 해독, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,6 +1,6 @@
 function solution(cipher, code) {
     var answer = '';
-    for(let i=code-1;i<cipher.length;i= i+code){
+    for(let i=code-1;i<cipher.length;i+=code){
         answer += cipher[i];
     }
     return answer;
