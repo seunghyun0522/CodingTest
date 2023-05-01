e0410e55c1654a65af5100e144495abd32369c78
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-05-02 07:55:18 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,11 +1,5 @@
 function solution(my_string) {
     var answer = '';
-    my_string = my_string.split('');
-    for(let i = 0; i<my_string.length;i++){
-        if(my_string[i]>='a' && my_string[i]<='z'){
-            my_string[i] = my_string[i].toUpperCase();
-        }
-        else my_string[i]=my_string[i].toLowerCase();
-    }
-    return answer= my_string.join("");
+    for (let c of my_string) answer += c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase();
+    return answer;
 }
