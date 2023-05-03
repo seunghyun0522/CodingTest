8c9f4cd0cc5962583d5a98879cc2b0352744e23e
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-05-03 09:45:51 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a[6] ={1,1,2,2,2,8};
+    int b[6];
+    
+    for(int i=0;i<6;i++){
+        scanf("%d",&b[i]);
+    }
+    
+    for(int i=0;i<6;i++){
+        printf("%d ",a[i]-b[i]);
+    }
+}
