c51652423ac9e6c4ad35c79ff35131c85af2fa49
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-05-06 14:35:43 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,20 +1,13 @@
 function solution(numbers, direction) {
     var answer = [];
-    let tmp;
-    if(direction === "right"){
-        tmp = numbers[numbers.length-1];
-        for(let i=numbers.length-1;i>=0;i--){
-            numbers[i] = numbers[i-1];
-        }
-        numbers[0] = tmp;
-        
+
+    if ("right" == direction) {
+        numbers.unshift(numbers.pop());
+    } else {
+        numbers.push(numbers.shift());
     }
-    else if(direction ==="left"){
-        tmp = numbers[0];
-        for(let i=0;i<numbers.length-1;i++){
-            numbers[i] = numbers[i+1];
-        }
-        numbers[numbers.length-1] = tmp;
-    }
-    return answer=numbers;
+
+    answer = numbers;
+
+    return answer;
 }
