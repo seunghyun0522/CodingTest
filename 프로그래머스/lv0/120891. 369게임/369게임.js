a837e87650b94657017487400a11c418018a58b0
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-05-08 17:54:05 +0900

    [level 0] Title: 369게임, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -2,8 +2,5 @@ function solution(order) {
     let a= [];
     let cnt =0;
     a = order.toString().split('');
-    for(let i=0;i<a.length;i++){
-        if(a[i]!=0 && a[i]%3===0) cnt ++;
-    }
-    return cnt;
+    return a.filter((x)=> x!=0 && x%3===0).length;
 }
