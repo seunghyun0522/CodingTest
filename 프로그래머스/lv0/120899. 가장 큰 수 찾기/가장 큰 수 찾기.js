78460b23884bb58f0223921973fb391b2ba75647
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-05-08 09:53:35 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,11 +1,4 @@
 function solution(array) {
-    var answer = [];
-    
     let max = Math.max(...array);
-    answer.push(max);
-    for(let i=0; i<array.length;i++ ){
-        if(max === array[i]) answer.push(i);
-    }
-
-    return answer;
+    return [max, array.indexOf(max)];
 }
