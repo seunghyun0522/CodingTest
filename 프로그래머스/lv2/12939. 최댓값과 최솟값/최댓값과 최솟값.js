18744da447915733f32cef997a38848102583c5a
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-05-12 19:30:04 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,10 +1,4 @@
 function solution(s) {
-    let answer =[];
-    let a =s.split(" ").map(Number);
-    let max = Math.max(...a);
-    let min = Math.min(...a);
-        answer.push(min);
-    answer.push(max);
-
-    return answer.join(" ");
+    const arr = s.split(' ');
+    return Math.min(...arr) + ' '+Math.max(...arr);
 }
