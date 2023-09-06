85c61178e11e453e5750ca0fce5b917cef9451de
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-09-07 08:58:18 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,11 +1,9 @@
 function solution(n) {
-    var answer = 0;
     var sub=0;
     for(let i =1; i<=n;i++){
-        if(i*i === n) {sub = i+1;
-                              return sub*sub;}
-
-
+        if(i*i === n) {
+            sub = i+1;
+            return sub*sub;}
     }
     return -1;
 }
