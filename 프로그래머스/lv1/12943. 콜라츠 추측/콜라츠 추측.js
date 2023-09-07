091171e4c14677f6834caeeff58bfc8e44442258
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-09-07 09:38:10 +0900

    [level 1] Title: 콜라츠 추측, Time: 5.58 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,13 +1,10 @@
 function solution(num) {
-    let cnt = 0;
-    if(num===1) return 0;
-    while(cnt<=500){
-        if(num%2 ===0)  num/=2;
-        else num = num*3 +1;
+    var cnt = 0;
+    while(num!=1 && cnt!=500){
+ num%2==0 ? num = num/2 : num = num*3 +1;
+        console.log(num)
         cnt++;
-        if(num ===1) return cnt;
     }
-    return -1;
-}
+    return num ==1 ? cnt : -1;
 
-console.log(solution(1));
+}
