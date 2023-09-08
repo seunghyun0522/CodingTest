5c650228cd61fb87496c9e10a5751a1aa3421a8c
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-09-08 09:35:03 +0900

    [level 1] Title: 음양 더하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,8 @@
+function solution(absolutes, signs) {
+    let sum=0;
+    for(let i=0;i<signs.length;i++){
+        if(signs[i]===false) absolutes[i]*=-1;
+       sum+=absolutes[i];
+    }
+    return sum;
+}
