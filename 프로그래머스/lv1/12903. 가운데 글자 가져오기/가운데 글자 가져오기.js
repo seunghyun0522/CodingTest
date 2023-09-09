9e2f0c388e94d743278df48d1cb977e7db4dea7e
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-09-09 09:38:52 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,10 +1,8 @@
 function solution(s) {
-    
-    let arr = s.split('');
-    let key = Math.floor(arr.length/2);
 
-    if(arr.length % 2 ===0) return [arr[key-1],arr[key]].join('');
-    else return arr[key];
-    return arr;
+    let key = Math.floor(s.length/2);
+
+    return s.length % 2 ===0 ? s[key-1]+s[key] : s[key];
+
 
 }
