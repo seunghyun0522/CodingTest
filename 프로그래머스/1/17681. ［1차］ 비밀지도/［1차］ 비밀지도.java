0479237ff8d923d7a1a4bab3b33b793873ff51d3
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-12-27 12:32:31 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.31 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for(int i=0; i<n;i++){
+        answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+        answer[i] = "0".repeat(n - answer[i].length()) + answer[i];
+        answer[i] = answer[i].replaceAll("0"," ").replaceAll("1","#");
+
+
+ 
+            
+        }
+
+        return answer;
+    }
+}
