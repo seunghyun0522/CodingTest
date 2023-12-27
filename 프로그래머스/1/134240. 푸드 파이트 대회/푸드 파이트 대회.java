2d3cd37a9480081347afa990d40525d9ab1f530b
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2023-12-27 11:59:39 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 12.52 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,40 +1,14 @@
-import java.util.*;
+
 class Solution {
     public String solution(int[] food) {
-        int len=0;
-        
-        String answer = "";
-        for(int i=1;i<food.length;i++){
-            if(food[i] >1) {len += (food[i]%2 ==0) ? food[i] : food[i]-1; }
-      
-        }
-        len +=1;
-        int[] ans = new int[len];
-        
-        ans[len/2] = 0;
-       
-        int cnt =1;
-        int i=0;
-        int j=len-1;
+        String answer = "0";
 
-        while(cnt < food.length){
-            if(food[cnt]%2!=0) food[cnt]--;
-
-            while(food[cnt] > 0){
- 
-                ans[i] = cnt;
-                ans[j] =cnt;
-                i++;
-                j--;
-                food[cnt]-=2;
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
             }
-            cnt++;
         }
-        
-        
-        for(int value : ans){
-            System.out.print(value);
-        }
-        return answer = Arrays.toString(ans).replaceAll("[^0-9]","");
+
+        return answer;
     }
 }
