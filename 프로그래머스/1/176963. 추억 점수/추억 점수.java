de42294a48a6473f5f44bb4d8326412e287f1543
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-01-03 11:35:22 +0900

    [level 1] Title: 추억 점수, Time: 0.04 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,15 +1,28 @@
+import java.util.*;
+
 class Solution {
-	public int[] solution(String[] name, int[] yearning, String[][] photo) {
-		int[] answer = new int[photo.length];
-			for (int i = 0; i < photo.length; i++) {
-				for (int j = 0; j < photo[i].length; j++) {
-					for(int k=0;k<name.length;k++) {
-						if(photo[i][j].equals(name[k])) answer[i]+=yearning[k];
-					}
-				}
-			}
-			
-			return answer;
-		
-	}
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> nameYearningMap = new HashMap<>();
+        int nameLength = name.length;
+
+        // 이름과 yearning 값을 해시맵에 매핑
+        for (int i = 0; i < nameLength; i++) {
+            nameYearningMap.put(name[i], yearning[i]);
+        }
+
+        int photoLength = photo.length;
+        int[] answer = new int[photoLength];
+
+        // 각 사진에 대해 이름 확인 및 yearning 값 누적
+        for (int i = 0; i < photoLength; i++) {
+            for (int j = 0; j < photo[i].length; j++) {
+                String currentName = photo[i][j];
+                if (nameYearningMap.containsKey(currentName)) {
+                    answer[i] += nameYearningMap.get(currentName);
+                }
+            }
+        }
+
+        return answer;
+    }
 }
