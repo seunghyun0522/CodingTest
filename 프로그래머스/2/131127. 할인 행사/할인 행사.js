40ec74a83506dafc5ae935e5973448aeeb28034d
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-01-11 17:22:40 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,10 +1,11 @@
 function solution(want, number, discount) {
   let start = 0;
   let end = 10;
-  let flag = true;
   let cnt = 0;
+
   while (end <= discount.length) {
     let tmp = [...number];
+
     for (let i = 0; i < want.length; i++) {
       for (let j = start; j < end; j++) {
         if (want[i] === discount[j]) {
@@ -12,16 +13,22 @@ function solution(want, number, discount) {
         }
       }
     }
+
+    let flag = true;
     for (let i = 0; i < tmp.length; i++) {
       if (tmp[i] > 0) {
         flag = false;
         break;
       }
     }
-    if (flag) {    cnt++; }
+
+    if (flag) {
+      cnt++;
+    }
+
     start++;
     end++;
-    flag = true;
   }
+
   return cnt;
 }
