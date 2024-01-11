5beabf525fa5028942f4c5b8b3a1821715f962d4
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-01-11 17:35:16 +0900

    [level 2] Title: 할인 행사, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,34 +1,42 @@
 function solution(want, number, discount) {
-  let start = 0;
-  let end = 10;
-  let cnt = 0;
+  const windowSize = 10;
+  let count = 0;
+  let remainingNumbers = [...number];
+  let wantCount = want.reduce((acc, curr) => {
+    acc[curr] = (acc[curr] || 0) + 1;
+    return acc;
+  }, {});
 
-  while (end <= discount.length) {
-    let tmp = [...number];
-
-    for (let i = 0; i < want.length; i++) {
-      for (let j = start; j < end; j++) {
-        if (want[i] === discount[j]) {
-          tmp[i]--;
-        }
-      }
+  // 초기 창구의 상태를 계산
+  for (let i = 0; i < windowSize; i++) {
+    const item = discount[i];
+    if (item in wantCount) {
+      remainingNumbers[want.indexOf(item)]--;
     }
-
-    let flag = true;
-    for (let i = 0; i < tmp.length; i++) {
-      if (tmp[i] > 0) {
-        flag = false;
-        break;
-      }
-    }
-
-    if (flag) {
-      cnt++;
-    }
-
-    start++;
-    end++;
   }
 
-  return cnt;
+  // 초기 창구에서 가능한 경우 카운트 증가
+  if (remainingNumbers.every(num => num <= 0)) {
+    count++;
+  }
+
+  // 슬라이딩 윈도우를 적용하여 창구의 상태를 업데이트하면서 카운트 증가
+  for (let end = windowSize; end < discount.length; end++) {
+    const newItem = discount[end];
+    const oldItem = discount[end - windowSize];
+
+    if (newItem in wantCount) {
+      remainingNumbers[want.indexOf(newItem)]--;
+    }
+    if (oldItem in wantCount) {
+      remainingNumbers[want.indexOf(oldItem)]++;
+    }
+
+    // 현재 창구에서 가능한 경우 카운트 증가
+    if (remainingNumbers.every(num => num <= 0)) {
+      count++;
+    }
+  }
+
+  return count;
 }
