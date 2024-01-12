c9c9109a87d0565a0f4d84dc6d16165204421dbd
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-01-12 15:31:06 +0900

    [level Lv.1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,29 @@
+function solution(nums) {
+  var answer = -1;
+
+  nums.sort();
+
+
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        let num = nums[i] + nums[j] + nums[k];
+        if (prime(num)) count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+function prime(num) {
+  let flag = true;
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+      flag = false;
+      break;
+    }
+  }
+  return flag;
+}
