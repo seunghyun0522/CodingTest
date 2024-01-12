a24ebd193308f878673775f8c4e0fe68bb625848
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-01-12 10:06:13 +0900

    [level Lv.1] Title: 폰켓몬, Time: 0.32 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.js
@@ -1,11 +1,4 @@
 function solution(nums) {
-  let len1 = nums.length / 2;
-
-  const set = new Set(nums);
-  const arr = [...set];
-  len2 = arr.length;
-
-  let answer = len1 > len2 ? len2 : len1;
-
-  return answer;
+  const arr = [...new Set(nums)];
+  return nums.length/2 > arr.length ? arr.length : nums.length/2;
 }
