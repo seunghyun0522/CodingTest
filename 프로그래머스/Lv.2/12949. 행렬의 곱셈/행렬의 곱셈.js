3fa80e5eb3085211bfc84b703d64f2f98c195485
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-01-15 14:33:49 +0900

    [level Lv.2] Title: 행렬의 곱셈, Time: 2.97 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/Lv.2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -5,7 +5,7 @@ function solution(arr1, arr2) {
         let result = [];
         for(let j = 0; j < arr2[0].length; j++) {
             let elem = 0;
-            for(let k = 0; k < arr2.length; k++) { // arr1[0].length도 가능.
+            for(let k = 0; k < arr2.length; k++) { 
                 elem += arr1[i][k] * arr2[k][j];
             }
             result.push(elem);
