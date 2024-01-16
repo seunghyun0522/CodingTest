9761faf04d7640762c07d1f8c4f3fcdba1745a49
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-01-16 15:55:26 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.js
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.js
@@ -1,7 +1,4 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().trim();
-
-// input을 반대로 뒤집어줍니다.
 const reverse = input.split("").reverse().join("");
 
-// input과 reverse가 같다면 1을, 아니라면 0을 출력합니다.
 console.log(input === reverse ? 1 : 0);
