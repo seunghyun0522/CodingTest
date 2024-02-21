b448f72ef96895f5824c4b44bb238596a39968de
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-02-21 20:52:52 +0900

    [level 0] Title: 배열 만들기 4, Time: 2.98 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,20 @@
+function solution(arr) {
+    var stk = [];
+    var i =0;
+    while(i < arr.length){
+    if(stk.length<1) 
+    {
+        stk.push(arr[i]);
+        i++;
+    }else{
+        if(stk[stk.length-1] < arr[i]){
+            stk.push(arr[i]);
+            i++;
+        }else{
+            stk.pop();
+        }
+        
+    }
+    }
+    return stk;
+}
