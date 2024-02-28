db41a0fab4b314d35af1b1b5d176ca7a704f225e
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-02-28 12:04:31 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
@@ -0,0 +1,6 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+
+const A = input[0];     
+const B = input[1];     
+const V = input[2];     
+console.log(Math.ceil((V - B) / (A - B)));
