f222e6481bacac93c29c252559d30f174d098a9b
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-03-03 18:23:54 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11328 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,21 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+		
+		
+		//         ,r'"7
+		//r`-_   ,'  ,/
+		// \. ". L_r'
+		//   `~\/
+		//      |
+		//      |
+	}
+
+}
