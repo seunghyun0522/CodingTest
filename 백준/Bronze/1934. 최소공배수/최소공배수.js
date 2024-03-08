8600017bc4d28ed59c594d28926fb5541256551a
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-03-08 10:52:21 +0900

    [Bronze I] Title: 최소공배수, Time: 492 ms, Memory: 12260 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.js
@@ -0,0 +1,31 @@
+const input = (
+  process.platform == "linux"
+    ? require("fs").readFileSync("/dev/stdin").toString()
+    : require("fs").readFileSync("예제.txt").toString()
+)
+  .trim()
+  .split("\n");
+
+let n = parseInt(input[0]);
+
+for (let i = 1; i < input.length; i++) {
+  let [a, b] = input[i].split(" ").map(Number);
+  let result = lcm(a, b);
+  console.log(result);
+}
+
+function lcm(a, b) {
+  let lcm;
+  let gcd;
+  let m = a > b ? a : b;
+
+  for (let i = m; i >= 1; i--) {
+    if (a % i === 0 && b % i === 0) {
+      gcd = i;
+      break;
+    }
+  }
+  lcm = gcd * parseInt(a / gcd) * parseInt(b / gcd);
+
+  return lcm;
+}
