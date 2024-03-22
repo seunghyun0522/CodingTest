e5a88d2abc76f356e874ce5ebb4edd432f397a19
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-03-22 15:05:52 +0900

    [level 0] Title: 수 조작하기 2, Time: 659.79 ms, Memory: 378 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,14 +1,6 @@
 class Solution {
     public String solution(int[] numLog) {
-               String answer = "";
-        for(int i=0; i<numLog.length-1;i++){
-            int cal = numLog[i+1] -numLog[i];
-            if(cal == 1) answer+="w";
-            else if(cal == -1) answer+='s';
-            else if(cal == 10) answer+='d';
-            else answer+='a';
-        }
- 
+        String answer = "";
         return answer;
     }
 }
