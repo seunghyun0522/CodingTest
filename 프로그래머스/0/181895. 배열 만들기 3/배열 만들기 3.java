ed0418510d31ce5952d96eb61d69db0e94941793
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-03-26 20:33:31 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.16 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int[] answer = new int[intervals[0][1]-intervals[0][0] + intervals[1][1] -intervals[1][0]+2];
+        int cnt=0;
+        for(int i=intervals[0][0];i<=intervals[0][1];i++){
+            answer[cnt] = arr[i];
+            cnt++;
+        }
+        for(int i=intervals[1][0]; i<=intervals[1][1]; i++){
+            answer[cnt] = arr[i];
+            cnt++;
+        }
+        return answer;
+    }
+}
