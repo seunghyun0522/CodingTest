40f26ed4557cbe5df109af049f656589c1791270
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-03-27 12:46:52 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.29 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/README.md
+++ 프로그래머스/2/87377. 교점에 별 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 교점에 별 만들기 - 87377 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377?language=java) 
 
 ### 성능 요약
 
-메모리: 83 MB, 시간: 11.25 ms
+메모리: 79.1 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 25일 15:38:06
+2024년 03월 27일 12:46:51
 
 ### 문제 설명
 
--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,87 +1,82 @@
 import java.util.*;
+
+
 class Solution {
     
     private static class Point{
         public final long x, y;
+        
         private Point(long x, long y){
             this.x = x;
-            this.y = y;
+            this.y =y;
         }
     }
     
-    
     private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2){
-        double x = (double)(b1 * c2 - b2*c1) / (a1*b2 - a2*b1);
-        double y = (double)(a2*c1 - a1*c2) / (a1 * b2 - a2 * b1);
+        double x = (double) (b1*c2 - b2 * c1) / (a1 *b2 - a2 * b1);
+        double y = (double) (a2*c1 - a1 * c2) / (a1 *b2 - a2 * b1);
         
         if(x%1 != 0 || y%1 !=0) return null;
         
-        return new Point((long) x , (long) y);
-    }
-    
-    private Point getMinimumPoint(List<Point> points){
+        return new Point((long)x , (long)y);
         
+    }
+    private Point getMinimumPoint(List<Point> points ){
         long x = Long.MAX_VALUE;
         long y = Long.MAX_VALUE;
         
         for(Point p : points){
-            if(p.x < x) x=p.x;
+            if(p.x < x) x= p.x;
             if(p.y < y) y = p.y;
         }
+        
         return new Point(x,y);
     }
-    private Point getMaximumPoint(List<Point> points){
-  
+    
+     private Point getMaximumPoint(List<Point> points ){
         long x = Long.MIN_VALUE;
         long y = Long.MIN_VALUE;
         
         for(Point p : points){
-            if(p.x > x) x=p.x;
+            if(p.x > x) x= p.x;
             if(p.y > y) y = p.y;
         }
+        
         return new Point(x,y);
     }
-    
-    
     public String[] solution(int[][] line) {
-        String[] answer = {};
         
-        List<Point> points = new ArrayList<>();
+        List <Point> points = new ArrayList<>();
+        
         for(int i=0; i<line.length;i++){
-            for(int j=i+1; j<line.length;j++){
-                Point intersection = intersection(line[i][0],line[i][1],line[i][2],line[j][0],line[j][1],line[j][2]);
+            for(int j=i+1;j<line.length;j++){
+                Point intersection = intersection(line[i][0], line[i][1],line[i][2],line[j][0],line[j][1],line[j][2]);
                 
-                if(intersection != null)  points.add(intersection);
-                
-   
-     
+                if(intersection != null) points.add(intersection);
             }
         }
-                Point maximum = getMaximumPoint(points);
-                Point minimum = getMinimumPoint(points);
-    
-                int width = (int) (maximum.x - minimum.x +1);
-                int height = (int) (maximum.y - minimum.y+1);
-                
-                System.out.println(width + " " + height);
-                char [][] arr = new char[height][width];
+        
+        Point minimum = getMinimumPoint(points);
+        Point maximum = getMaximumPoint(points);
+        
+        int width = (int) (maximum.x -minimum.x +1);
+        int height = (int) (maximum.y -minimum.y +1);
+        
+        char [][] arr = new char[height][width];
         
         for(char[] row : arr){
             Arrays.fill(row,'.');
         }
+        
         for(Point p : points){
-            int x = (int) (p.x - minimum.x);
-            int y = (int) (maximum.y - p.y);
+            int x = (int)(p.x - minimum.x);
+            int y = (int)(maximum.y- p.y);
             arr[y][x] = '*';
         }
-        
-        String[] result = new String[arr.length];
-        for(int i=0; i<result.length;i++){
-            result[i] = new String(arr[i]);
+        String[] answer = new String[arr.length];
+        for(int i=0; i<answer.length;i++){
+            answer[i] = new String(arr[i]);
         }
-        
-        return result;
-        
-
+        return answer;
     }
 }
