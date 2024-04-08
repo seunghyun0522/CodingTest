4c128da1afcfe69c562f0664be81ca8135daa5bf
seung kim <75532258+seunghyun0522@users.noreply.github.com>
2024-04-08 18:38:13 +0900

    [level 0] Title: 문자열 뒤집기, Time: 13.42 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -2,16 +2,13 @@ class Solution {
     public String solution(String my_string, int s, int e) {
         String answer = "";
 
-        int count = 0;
-        for(int i = 0; i < my_string.length(); i++) {
-            if(i >= s && i <= e) {
-                answer += my_string.charAt(e-count);
-                count++;
-            } else {
-                answer += my_string.charAt(i);
-            }
+        answer += my_string.substring(0,s);
+        
+        for(int i=e; i>=s;i--){
+            answer += my_string.charAt(i);
         }
-
+        
+        answer += my_string.substring(e+1);
         return answer;
     }
 }
