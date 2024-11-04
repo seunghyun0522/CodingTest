7126424e07aa411b4f20dabc8a89a1b97ffb5f78
seunghyun kim <75532258+seunghyun0522@users.noreply.github.com>
2024-11-04 22:45:09 +0900

    [Bronze II] Title: 분해합, Time: 184 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -8,22 +8,19 @@ public class Main{
    Scanner in = new Scanner(System.in);
 
         int N = in.nextInt();
-
-        int result = 0;
-
-        for(int i = 0; i< N; i++){
-            int number = i;
-            int sum = 0;
-
-            while(number != 0){
-                sum += number % 10;
-                number /= 10;
+        int result =0;
+        for(int i=0; i<N;i++){
+            int num = i;
+            int sum=0;
+            while(num>0){
+                sum += num %10;
+                num/=10;
             }
-
             if(sum + i == N){
-                result = i;
+                result =i;
                 break;
             }
+  
         }
         System.out.println(result);
         
