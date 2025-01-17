ca23469bce8b2dbcdaab2b980edb12ad9ea064e0
seunghyun kim <75532258+seunghyun0522@users.noreply.github.com>
2025-01-17 13:00:20 +0900

    [Bronze III] Title: 0 = not cute / 1 = cute, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+not_cute = 0
+cute = 0
+
+for i in range(n):
+    vote = int(input())
+    if(vote == 1):
+        cute += 1
+    else:
+        not_cute += 1
+
+print("Junhee is cute!" if cute > not_cute else "Junhee is not cute!")
